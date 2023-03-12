607ec709dffaf2f20cb0a7407e35f62a52856c42
NickShelud <N.She.1997@mail.ru>
2023-03-12 10:15:05 +0300

    step 2

--- public/index.php
+++ public/index.php
@@ -1,3 +1,29 @@
 <?php
 
-echo 'done';
+// Подключение автозагрузки через composer
+require __DIR__ . '/../vendor/autoload.php';
+
+use Slim\Factory\AppFactory;
+use DI\Container;
+use Slim\Middleware\MethodOverrideMiddleware;
+use Root\HexletSlimExample\Validator;
+
+session_start();
+
+$container = new Container();
+$container->set('renderer', function () {
+    return new Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+$container->set('flash', function () {
+    return new Slim\Flash\Messages();
+});
+
+$app = AppFactory::createFromContainer($container);
+$app->add(MethodOverrideMiddleware::class);
+$app->addErrorMiddleware(true, true, true);
+
+
+$app->get('/', function ($request, $response) {
+    $params = [];
+    return $this->get('renderer')->render($response, 'index.phtml', $params);
+})->setName('startPage');
