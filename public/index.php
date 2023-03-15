ed844cf7e074b6133a227aa0c1dd87f5bcd0b39a
NickShelud <N.She.1997@mail.ru>
2023-03-15 08:03:53 +0300

    step 3

--- public/index.php
+++ public/index.php
@@ -7,21 +7,32 @@ use Slim\Factory\AppFactory;
 use DI\Container;
 use Slim\Middleware\MethodOverrideMiddleware;
 use Root\HexletSlimExample\Validator;
+use Hexlet\Code\Connection;
 
 session_start();
+
+try {
+    Connection::get()->connect();
+    echo 'A connection to the PostgreSQL database sever has been established successfully.';
+} catch (\PDOException $e) {
+    echo $e->getMessage();
+}
 
 $container = new Container();
 $container->set('renderer', function () {
     return new Slim\Views\PhpRenderer(__DIR__ . '/../templates');
 });
 
+$container->set('flash', function () {
+    return new Slim\Flash\Messages();
+});
+
 $app = AppFactory::createFromContainer($container);
 $app->add(MethodOverrideMiddleware::class);
 $app->addErrorMiddleware(true, true, true);
 
-
 $app->get('/', function ($request, $response) {
     return $this->get('renderer')->render($response, 'index.phtml');
 });
-
+//phpinfo();
 $app->run();
