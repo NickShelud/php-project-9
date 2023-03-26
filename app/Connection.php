55da6c1b015a6ce159a00190d0663caf6844ba09
NickShelud <N.She.1997@mail.ru>
2023-03-26 09:10:17 +0300

    fix bugs

--- app/Connection.php
+++ app/Connection.php
@@ -20,8 +20,18 @@ final class Connection
      */
     public function connect()
     {
-        // чтение параметров в файле конфигурации ini
-        $params = parse_ini_file('database.ini');
+        if (getenv('DATABASE_URL')) {
+            $databaseUrl = parse_url(getenv('DATABASE_URL'));
+        }
+        if (isset($databaseUrl['host'])) {
+            $params['host'] = $databaseUrl['host'];
+            $params['port'] = isset($databaseUrl['port']) ? $databaseUrl['port'] : null;
+            $params['database'] = isset($databaseUrl['path']) ? ltrim($databaseUrl['path'], '/') : null;
+            $params['user'] = isset($databaseUrl['user']) ? $databaseUrl['user'] : null;
+            $params['passw'] = isset($databaseUrl['pass']) ? $databaseUrl['pass'] : null;
+        } else {
+            $params = parse_ini_file('database.ini');
+
         if ($params === false) {
             throw new \Exception("Error reading database configuration file");
         }
