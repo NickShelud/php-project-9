e781188614b68f1325e9dea7f6ccf67e72e06596
NickShelud <N.She.1997@mail.ru>
2023-03-28 13:04:17 +0300

    fix bugs

--- src/CreateTable.php
+++ src/CreateTable.php
@@ -17,7 +17,7 @@ class CreateTable
      * инициализация объекта с объектом \PDO
      * @тип параметра $pdo
      */
-    public function __construct($pdo)
+    public function __construct(object $pdo)
     {
         $this->pdo = $pdo;
     }
--- src/PgsqlData.php
+++ src/PgsqlData.php
@@ -14,7 +14,7 @@ class PgsqlData
      * инициализация объекта с объектом \PDO
      * @тип параметра $pdo
      */
-    public function __construct($pdo)
+    public function __construct(object $pdo)
     {
         $this->pdo = $pdo;
     }
