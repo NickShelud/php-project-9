677ae06bb17ed4e009b444ddd69ce793bd8d2ee1
NickShelud <N.She.1997@mail.ru>
2023-04-06 19:30:36 +0300

    update check page

--- public/index.php
+++ public/index.php
@@ -138,9 +138,9 @@ $app->post('/urls/{url_id}/checks', function ($request, $response, $args) use ($
         $this->get('flash')->addMessage('success', 'Страница успешно проверена');
     } catch (TransferException $e) {
         $this->get('flash')->addMessage('failure', 'Произошла ошибка при проверке, не удалось подключиться');
-        $url = $router->urlFor('urlsId', ['id' => $url_id]);
+        //$url = $router->urlFor('urlsId', ['id' => $url_id]);
         //$newResponse = $response->withStatus(422);
-        return $response->withRedirect($url);
+        //return $response->withRedirect($url);
     }
 
     $document = new Document($name[0]['name'], true);
