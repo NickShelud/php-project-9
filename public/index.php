95ea747f1011207960fe2b0c5278810af9d1c460
NickShelud <N.She.1997@mail.ru>
2023-04-09 20:02:54 +0300

    update index.php

--- public/index.php
+++ public/index.php
@@ -135,11 +135,11 @@ $app->post('/urls/{url_id}/checks', function ($request, $response, $args) use ($
         $client = new Client();
         $res = $client->request('GET', $name[0]['name']);
         $checkUrl['status'] = $res->getStatusCode();
-        if ($checkUrl['status'] == 403) {
-            $this->get('flash')->addMessage('success', 'Проверка была выполнена успешно, но сервер ответил с ошибкой');
-        } elseif ($checkUrl['status'] == 200) {
-            $this->get('flash')->addMessage('success', 'Страница успешно проверена');
-        }
+      //if ($checkUrl['status'] == 403) {
+      //    $this->get('flash')->addMessage('success', 'Проверка была выполнена успешно, но сервер ответил с ошибкой');
+      //} elseif ($checkUrl['status'] == 200) {
+      //    $this->get('flash')->addMessage('success', 'Страница успешно проверена');
+      //}
     } catch (TransferException $e) {
         $this->get('flash')->addMessage('failure', 'Произошла ошибка при проверке, не удалось подключиться');
         $url = $router->urlFor('urlsId', ['id' => $url_id]);
