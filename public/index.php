7775473e1c677e2261ffc5884b14f3ce2ee154d9
NickShelud <N.She.1997@mail.ru>
2023-04-09 12:12:37 +0300

    update index.php

--- public/index.php
+++ public/index.php
@@ -137,7 +137,7 @@ $app->post('/urls/{url_id}/checks', function ($request, $response, $args) use ($
         $checkUrl['status'] = $res->getStatusCode();
         //var_dump($checkUrl['status']);
         //$this->get('flash')->addMessage('success', 'Страница успешно проверена');
-    } catch (TransferException $e) {
+    } catch (ClientException $e) {
         $this->get('flash')->addMessage('failure', 'Произошла ошибка при проверке, не удалось подключиться');
         //$url = $router->urlFor('urlsId', ['id' => $url_id]);
         //$newResponse = $response->withStatus(422);
